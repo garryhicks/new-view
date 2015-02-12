d61f88761996d6d83ee7012951d040e3145893b3
Garry Hicks <garryhicks21@gmail.com>
2015-02-12 04:06:52 +0000

    interval set

--- application.js
+++ application.js
@@ -1,19 +1,28 @@
+var interval;
 $(document).ready(function(){
   $('.container').on("click", "#fetch-button", function(){
-    $.ajax({
-      type: 'GET',
-      dataType: 'jsonp',
-      url: "https://api.instagram.com/v1/tags/lighthouse/media/recent?access_token=1700796495.1fb234f.2b336c55a90147a79124f80baaa5f868",
-      success: function(data){
-        for (var i = 0; i < 50; i++) {
-          var $img = $('<img>');
-          $img.attr('src', data.data[i].images.standard_resolution.url);
-          $('#picture-frame').append($img);
+
+    var poll = function() {
+      $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        url: "https://api.instagram.com/v1/tags/lighthouse/media/recent?access_token=1700796495.1fb234f.2b336c55a90147a79124f80baaa5f868",
+        success: function(data){
+          $('#picture-frame').empty();
+          for (var i = 0; i < 20; i++) {
+            var $img = $('<img>');
+            console.log(data);
+            $img.attr('src', data.data[i].images.standard_resolution.url);
+            $('#picture-frame').append($img);
+          }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR)
         }
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR)
-      }
-    });
+      });
+    }
+    window.clearInterval(interval)
+    interval = setInterval(poll, 10000);
+    poll();
   });
 });
